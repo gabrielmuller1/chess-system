0d893d43f2a343c54293958f6dc1b453106eb4cd
Gabriel Muller <gabrielmuller708@gmail.com>
2021-11-25 14:22:30 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class ChessException extends RuntimeException {
+
+	private static final long serielVersionUID = 1L;
+
+	public ChessException(String msg) {
+		super(msg);
+	}
+
+}
